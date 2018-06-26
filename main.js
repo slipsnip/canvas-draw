65917edccae187c698ab6decf7c01b9bc0a27774
Michael Esch <eschmichael@yandex.com>
2018-06-25 23:20:26 -0400

    ESLint fix

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const electron = require('electron');
+const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
@@ -9,25 +9,25 @@ function createWindow () {
     mainWindow = new BrowserWindow({width: 800, height: 600});
 
     // and load the index.html of the app.
-    mainWindow.loadURL('localhost:4000');
+    mainWindow.loadURL("localhost:4000");
 
-    mainWindow.on('closed', function () {
+    mainWindow.on("closed", function () {
         mainWindow = null;
-    })
+    });
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Mac stuff.
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
+app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") {
         app.quit();
     }
-})
+});
 
 // Mac stuff...
-app.on('activate', function () {
+app.on("activate", function () {
     if (mainWindow === null) {
         createWindow();
     }
-})
+});
