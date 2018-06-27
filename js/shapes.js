120d0804216f5c0fc44a87c6b21f514746360e53
Michael Esch <eschmichael@yandex.com>
2018-06-26 20:54:56 -0400

    Playing with arc's and circles

--- js/shapes.js
+++ js/shapes.js
@@ -1,4 +1,16 @@
+/** Class for rounded rectangle */
 class roundRect {
+    /**
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} width 
+     * @param {number} height 
+     * @param {number} radius 
+     * @param {string} strokeStyle // MDN: CanvasRenderingContext2D.strokeStyle
+     * @param {number} lineWidth  // Thickness of line
+     * @param {Array} dash // MDN: Array of numbers which specify distances to alternately draw a line and a gap
+     */
     constructor(x, y, width, height, radius, strokeStyle = "#000", lineWidth = 2, dash = [0, 0]) {
         this.x = x;
         this.y = y;
@@ -9,7 +21,10 @@ class roundRect {
         this.lineWidth = lineWidth;
         this.dash = dash;
     }
-
+    /**
+     * 
+     * @param {CanvasRenderingContext2D} ctx // MDN: CanvasRenderingContext2D
+     */
     draw(ctx) {
         ctx.beginPath();
         ctx.moveTo(this.x + this.radius, this.y);
@@ -33,13 +48,31 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 canvas.style.border = "2px solid black";
 
-const actionRect = new roundRect(100, 100,300,300,25,"#f00");
-actionRect.draw(ctx);
+// Test rounded rectangle class
+// const smoothRectangle = new roundRect(100, 100,300,300,25,"#f00");
+// smoothRectangle.draw(ctx);
 
+// Experiment with drawing lines, setting styles
+// ctx.beginPath();
+// ctx.moveTo(50,300);
+// ctx.arcTo(300,100, 400, 300, 15);
+// ctx.strokeStyle = "#fa34bc";
+// ctx.setLineDash([4,4]);
+// ctx.lineWidth = 5;
+// ctx.stroke();
+
+// Playing with arcs and circles
 ctx.beginPath();
-ctx.moveTo(50,300);
-ctx.arcTo(300,100, 400, 300, 15);
-ctx.strokeStyle = "#fa34bc";
-ctx.setLineDash = [4,4];
-ctx.lineWidth = 5;
+ctx.arc(235,235,130,0,1,true);
+ctx.strokeStyle = "#0f0";
+ctx.lineWidth = 2;
+ctx.setLineDash([2,2]);
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(235,235);
+
+// Draw radius line
+ctx.lineTo(235+130,235);
+ctx.setLineDash([0,0]);
+ctx.strokeStyle = "#000";
 ctx.stroke();
