ee341b107835d2985b3178c4fe07e19027203602
Michael Esch <eschmichael@yandex.com>
2018-06-27 21:05:33 -0400

    Add window dimensions to title on resize

--- main.js
+++ main.js
@@ -6,17 +6,25 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
 function createWindow () {
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({width: 800, height: 600, title: "Canvas API Draw : [800 - 600]"});
 
     mainWindow.loadURL(`file://${__dirname}/index.html`);
-
+    mainWindow.on("resize", changeTitle);
 
     mainWindow.on("closed", function () {
         mainWindow = null;
     });
 }
 
+function changeTitle ()
+{
+    const size = mainWindow.getSize();
+    const title = `${mainWindow.title} : [${size.join(" - ")}]`;
+    mainWindow.setTitle(title);
+}
+
 app.on("ready", createWindow);
+
 
 // Mac stuff.
 app.on("window-all-closed", function () {
